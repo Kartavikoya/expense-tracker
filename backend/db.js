cb3459be0c18d903df299bbaf46ca43de4007e0b
Kartavikoya <www.kartavikoya456@gmail.com>
2026-04-20 20:42:30 +0530

    done changes for mongodb

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/expense-tracker", {
+mongoose.connect("mongodb+srv://kartavimehta_db_user:waY65fXkGc2hnL0s@cluster0.tqg2e09.mongodb.net/expense-tracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
